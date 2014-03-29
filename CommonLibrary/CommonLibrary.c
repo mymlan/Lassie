99c2ce2119e1641f8204241186354a6309a16c82
mymlan <mikaela.ivars@gmail.com>
2014-03-29 14:40:21 +0100

    lagt till vårt allmänna bibliotek CommonLibrary

---
+++ CommonLibrary/CommonLibrary.c
@@ -0,0 +1,15 @@
+/*
+ * CommonLibrary.c
+ *
+ * Created: 2014-03-29 14:38:18
+ *  Author: Mymlan
+ */ 
+
+#include <avr/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code
+
+    return 0;
+}
