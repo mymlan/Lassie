2e8c2771ea3d29cd58c43a181d5f8ab7aa175264
mymlan <mikaela.ivars@gmail.com>
2014-03-29 14:26:57 +0100

    Skapat styr samt sensor-modul

---
+++ Sensor/Sensor.c
@@ -0,0 +1,17 @@
+/*
+ * Sensor.c
+ *
+ * Created: 2014-03-29 14:22:07
+ *  Author: Mymlan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ Steering/Steering.c
@@ -0,0 +1,17 @@
+/*
+ * Steering.c
+ *
+ * Created: 2014-03-29 14:24:46
+ *  Author: Mymlan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
