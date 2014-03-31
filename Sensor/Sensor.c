9dae6650586a72cbba0152db514b8ed37fb05499
gusno <gusno119@student.liu.se>
2014-03-31 15:19:51 +0200

    Sensor in da house

--- Sensor/Sensor.c
+++ Sensor/Sensor.c
@@ -2,16 +2,74 @@
  * Sensor.c
  *
  * Created: 2014-03-29 14:22:07
- *  Author: Mymlan
+ *  Author: HuGu
  */ 
 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+uint8_t sensor1, sensor2, sensor3, sensor4, sensor5;
+
+
+void init_ports(){
+	DDRA = 0xFF;
+	PORTB = 0xff;
+}
+
 
 int main(void)
+
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	init_ports();
+	ADCSRA |= 1<<ADPS2;
+	ADMUX |= 1<<REFS0 | 1<<REFS1 | 1<<ADLAR | 2;
+	ADCSRA |= 1<<ADIE;
+	ADCSRA |= 1<<ADEN;
+	
+	sei();
+	
+	ADCSRA | 1<<ADSC;
+	
+	
+	while(1)
+	{
+	}
+}
+
+ISR(ADC_vect)
+{
+	
+	
+	switch(ADMUX)
+	{
+		case 0xE0:
+		sensor1 = ADCH;
+		ADMUX = 0xE1;
+		break;
+		case 0xE1:
+		sensor2 = ADCH;
+		ADMUX = 0xE2;
+		break;
+		case 0xE2:
+		sensor3 = ADCH;
+		ADMUX = 0xE3;
+		break;
+		case 0xE3:
+		sensor4 = ADCH;
+		ADMUX = 0xE4;
+		break;
+		case 0xE4:
+		sensor5 = ADCH;
+		ADMUX = 0xE0;
+		break;
+		
+		
+	}
+	
+	
+	
+	ADCSRA | 1<<ADSC;
+	
+	
 }
