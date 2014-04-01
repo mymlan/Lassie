71d88ea3f8c5bb5f0de50e8c4b0a391eaadd6f64
mymlan <mikaela.ivars@gmail.com>
2014-04-01 11:57:16 +0200

    kompilerar! Första små funk för byte från komm till sensor

--- Communication/Communication.c
+++ Communication/Communication.c
@@ -2,12 +2,43 @@
  * Communication.c
  *
  * Created: 2014-03-29 14:10:04
- *  Author: Mymlan
+ *  Author: MikaLina
  */ 
-
 
 #include <avr/io.h>
 #include "../CommonLibrary/Common.h"
+
+
+/*//hur göra om det är flera bytes vi vill skicka?
+int number_of_bytes_in_data = 1; // räkna anta bytes, alt
+while(!(number_of_bytes_in_data == 0))
+{
+	Master_transmit_data(data_byte);
+	number_of_bytes_in_data = number_of_bytes_in_data - 1;
+}*/
+
+/* void Master_transmit_data_byte(unsigned char data_byte)
+*  Skiftar en byte i register mellan master och slave. Väntar på att överföring blir klar.
+*/
+void Master_transmit_data_byte(unsigned char data_byte)
+{
+	SPDR = data_byte;
+	while(!(SPSR & (1<<SPIF)))
+	{
+		;
+	}
+}
+
+/* Send_address_to_slave(unsigned char address_byte)
+*  Skickar adress-byte från master till sensor_slave
+*/
+int Send_address_to_sensor(unsigned char address_byte) 
+{
+	PORTB = (1<<PB4); //sätter SS låg
+	Master_transmit_data_byte(address_byte); //Skickar adressbyten till sensor
+	PORTB = (0<<PB4);
+	return 0;
+}
 
 int main(void)
 {
@@ -17,34 +48,10 @@ int main(void)
 	SPCR = 0xC0; //Aktiverar avbrott från SPI, aktiverar SPI, sätter modul till master. 
 	SPSR = 0x01; //Sätter SCK till fosc/2
 	
+	Send_address_to_sensor(0x02);
+	
     while(1)
     {
         //TODO:: Please write your application code 
     }
-}
-
-int Send_data_to_sensor(unsigned char address_byte, unsigned char data) //hur göra om det är flera bytes vi vill skicka?
-{
-	PORTB = (1<<PB4); //sätter SS låg
-	Master_transmit_data(address_byte); //Skickar adressbyten till sensor
-	//vänta på att sensor läst adressbiten och lagt in den info som sensor vill skicka i sitt SPDR
-	int number_of_bytes_in_data = 1; // räkna anta bytes, alt 
-	while(!(number_of_bytes_in_data == 0))
-	{
-		Master_transmit_data(data_byte);	
-		number_of_bytes_in_data = number_of_bytes_in_data - 1;
-	}
-	PORTB = (0<<PB4);
-	return 0;
-}
-
-//funktion som översätter map adress_byte vad int number_of_bytes_in_data blir
-
-void Master_transmit_data(unsigned char data_to_transmitt) //int eller void?
-{
-	SPDR = data_to_transmitt; //startar överföringen
-	while(!(SPSR & (1<<SPIF))) //vänta på att överföringen ska bli klar
-	{
-		;
-	}
 }
