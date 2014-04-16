df74bb0d16feb71403f9946757666c95f60a6485
hugggos <hugjo456@student.liu.se>
2014-04-16 14:24:06 +0200

    Lagt till funktioner i Sensor_cm_converter

---
+++ Sensor/Sensor_cm_converter.h
@@ -0,0 +1,26 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 2014-04-16 14:04:46
+ *  Author: Hugo
+ */ 
+
+
+#ifndef _SENSOR_CM_CONVERTER_H
+#define _SENSOR_CM_CONVERTER_H
+
+int S1_sensor_value_front(int n);
+
+int S2_sensor_value_front(int n);
+
+int S3_sensor_value_front(int n);
+
+int S4_sensor_value_front(int n);
+
+int S5_sensor_value_long(int n);
+
+
+
+
+
+#endif /* INCFILE1_H_ */
