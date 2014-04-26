ce8c40ca25bec32e0a72a268128c7b4e2627f45a
mymlan <mikaela.ivars@gmail.com>
2014-04-26 14:15:41 +0200

    lagt till parenteser så det inte kan bli fel med konstanter, samt definerat längd av sensor data

--- CommonLibrary/Common.h
+++ CommonLibrary/Common.h
@@ -1,20 +1,12 @@
-/*
- * CommonLibrary.c
- *
- * Created: 2014-03-29 14:38:18
- *  Author: Mymlan
- */ 
-
-//Exempel på macro
 
 #define COMMON_SET_PIN(PORT, PIN) (PORT |= (1 << PIN))
-// vänterskift med en etta, sendan or med register
+// vänterskift med en etta, sedan or med register
 
 #define COMMON_CLEAR_PIN(PORT, PIN) (PORT &= (~(1 << PIN)))
 // and med ettor och en nolla, på rätt plats
 
 #define COMMON_SET_BIT(REGISTER, BIT) (REGISTER |= (1 << BIT))
-// vänterskift med en etta, sendan or med register
+// vänterskift med en etta, sedan or med register
 
 #define COMMON_CLEAR_BIT(REGISTER, BIT) (REGISTER &= (~(1 << BIT)))
 // and med ettor och en nolla, på rätt plats
@@ -22,28 +14,29 @@
 #define COMMON_TOGGLE_PIN(PORT, PIN) (PORT ^= (1 << PIN))
 
 //ID-bytes:
-#define ID_BYTE_SENSOR_DATA 0x01
-#define ID_BYTE_DISTANCE 0x03
-#define ID_BYTE_ANGLE 0x06
+#define ID_BYTE_SENSOR_DATA (0x01)
+#define ID_BYTE_DISTANCE (0x03)
+#define ID_BYTE_ANGLE (0x06)
 
-#define ID_BYTE_GUIDED_DECISIONS 0x07
-#define ID_BYTE_AUTO_DECISIONS 0x08
+#define ID_BYTE_GUIDED_DECISIONS (0x07)
+#define ID_BYTE_AUTO_DECISIONS (0x08)
 
-#define ID_BYTE_GIVE_DISTANCE 0x02
-#define ID_BYTE_START_CALC_ANGLE 0x04
-#define ID_BYTE_GIVE_ANGLE 0x05
-#define ID_BYTE_GIVE_SENSOR_DATA 0x09
+#define ID_BYTE_GIVE_DISTANCE (0x02)
+#define ID_BYTE_START_CALC_ANGLE (0x04)
+#define ID_BYTE_GIVE_ANGLE (0x05)
+#define ID_BYTE_GIVE_SENSOR_DATA (0x09)
 
-#define MASTER_RECIEVED_BYTE 0x0A
+#define MASTER_RECIEVED_BYTE (0x0A)
 
 //Kommando till styrmodulen
-#define COMMAND_STOP 0x00
-#define COMMAND_FORWARD 0x01 
-#define COMMAND_BACKWARD 0x02
-#define COMMAND_TURN_RIGHT 0x03
-#define COMMAND_TURN_LEFT 0x04
-#define COMMAND_ROTATE_RIGHT 0x05
-#define COMMAND_ROTATE_LEFT 0x06
-#define COMMAND_CLOSE_CLAW 0x07
-#define COMMAND_OPEN_CLAW 0x08
+#define COMMAND_STOP (0x00)
+#define COMMAND_FORWARD (0x01)
+#define COMMAND_BACKWARD (0x02)
+#define COMMAND_TURN_RIGHT (0x03)
+#define COMMAND_TURN_LEFT (0x04)
+#define COMMAND_ROTATE_RIGHT (0x05)
+#define COMMAND_ROTATE_LEFT (0x06)
+#define COMMAND_CLOSE_CLAW (0x07)
+#define COMMAND_OPEN_CLAW (0x08)
 
+#define NUMBER_OF_BYTES_SENSOR_DATA (6)
