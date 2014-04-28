f47427d1538d4a7d367f130e234960c845991ef9
Denbe220 <denbe220@student.liu.se>
2014-04-28 15:49:02 +0200

    Steering uppdelad i två filer.

---
+++ Steering/Steering_functions.h
@@ -0,0 +1,27 @@
+﻿
+#ifndef _STEERING_FUNCTIONS_H
+#define _STEERING_FUNCTIONS_H
+
+void Initialize_interrupt();
+void Initialize_pwm();
+
+void Stop();
+void Forward();
+void Back();
+void Rotate_right();
+void Rotate_left();
+void Turn_right();
+void Turn_left();
+void Open_claw();
+void Close_claw();
+
+void Forward_regulated();
+void Back_regulated();
+
+void Delay_seconds(int seconds);
+void Testprogram();
+void gusfunk();
+void Loop();
+
+
+#endif
