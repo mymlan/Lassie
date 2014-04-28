1c30ec44bbcc11233bda6fd15485601b4b0ae3a9
Denbe220 <denbe220@student.liu.se>
2014-04-28 18:06:43 +0200

    Fix så funktionerna fungerar som tänkt, så dom verkar fungera nu. Kod för testning av antalet noder som får plats i minnet.

--- Communication/map_communication.c
+++ Communication/map_communication.c
@@ -24,6 +24,10 @@ uint8_t vinkelsensor = 1;
 int test_variable_a;
 int test_variable_b;
 //int robot_dir = 0; // nord, öst, syd, väst
+int NORTH = 0;
+int EAST = 1;
+int SOUTH = 2;
+int WEST = 3;
 
 //------ C, VI LOVAR ATT DESSA STRUCTAR DEFINIERAS -------//
 struct node_;
@@ -65,7 +69,7 @@ link* Newlink(int length_in, node* nodeptr)
 
 node* Newnode(int x_in, int y_in)
 {
-	node *p_node = malloc(sizeof(node)); // Allocerar minne
+	node *p_node = malloc(sizeof(node)); // Allokerar minne
 	
 	//for så många ööpningar, sen placera i array också, kanske if öppen på alla öppningar och directions, case i en case???
 	
@@ -82,6 +86,7 @@ node* Newnode(int x_in, int y_in)
 	p_node->links[1].open = 0;
 	p_node->links[2].open = 0;
 	p_node->links[3].open = 0;
+	
 	/*
 	//link arra[];
 	//p_node->links = arra;
@@ -89,6 +94,8 @@ node* Newnode(int x_in, int y_in)
 	p_node->links[1] = Newlink(1,NULL);
 	p_node->links[2] = Newlink(1,NULL);
 	*/
+	
+
 	return p_node;
 }
 
@@ -197,7 +204,6 @@ node* Create_node(int x, int y, node* p_robot_node, int robot_dir, int length, i
 	p_node->links[(robot_dir + 2) % 4].p_node = p_robot_node; // 3b. Koppla gammal nod till ny nod
 	p_node->links[(robot_dir + 2) % 4].length = length; // 3c. Längd för vägen bakåt satt
 	return p_node; // 4.
-	// UPPDATERA ROBOT DIR
 		// 5. Nollställ avstånd (kanske sker utanför funktionen)
 		// 6. Ta styrbslut och return (sker utanför funktionen)
 }
@@ -222,6 +228,7 @@ node* Create_origin(int open_walls)
 int Map_main(void)
 {
 	// Lite testning och pseudokod för hur allt kommer gå till
+	// FÖRKLARING: i what is open är 255 (avstånd) "öppet", och 0 (avstånd) "vägg". NORTH, WEST etc definierade som 0, 3 etc.
 	// [undersöker riktningar (säg 1100)]
 	node* p_robot_node = Create_origin(1100); // Skapa origo och robotpekare
 	// [ta strybelsut (låt oss åka norr)]
@@ -229,24 +236,40 @@ int Map_main(void)
 	// [beordra styrbeslut]
 	// [detekterar en helt ny nod]
 	// [ev. beräkning av längd (låt den bli 7)]
-	p_robot_node = Create_node(0, 7, p_robot_node, robot_dir, 7, What_is_open(0, 1, 0, robot_dir)); // Skapa nod
+	p_robot_node = Create_node(0, 7, p_robot_node, robot_dir, 7, What_is_open(0, 255, 0, robot_dir)); // Skapa nod
 	// (All information om kartan är sparad via robotpekaren och nås genom att följa robotpekaren)
 	// [loopa]
 	test_variable_a = p_robot_node->x; // Ska va 0
 	test_variable_b = p_robot_node->y; // Ska va 7
 	
-	p_robot_node = Create_node(3, 7, p_robot_node, robot_dir, 3, What_is_open(1, 1, 0, robot_dir)); // Vi har åkt höger och skapat en nod.
+	robot_dir = 1; // Vi åker öster
+	p_robot_node = Create_node(3, 7, p_robot_node, robot_dir, 3, What_is_open(255, 255, 0, robot_dir)); // Vi har åkt höger och skapat en nod.
 	test_variable_a = p_robot_node->x; // Ska va 3
 	test_variable_b = p_robot_node->y; // Ska va 7
 	
-	p_robot_node = Create_node(3, 9, p_robot_node, robot_dir, 3, What_is_open(1, 1, 0, robot_dir)); // Vi har åkt höger och skapat en nod.
-	test_variable_a = p_robot_node->links[2].p_node->x; // Ska va 3 (förra noden)
-	test_variable_b = p_robot_node->links[2].p_node->y; // Ska va 7 (förra noden)
+	robot_dir = 0; // Vi åker norr
+	p_robot_node = Create_node(3, 9, p_robot_node, robot_dir, 3, What_is_open(255, 255, 0, robot_dir)); // Vi har åkt vänster och skapat en nod.
+	test_variable_a = p_robot_node->links[SOUTH].p_node->links[WEST].p_node->x; // Ska va 0 (förrförra noden)
+	test_variable_b = p_robot_node->links[SOUTH].p_node->links[WEST].p_node->y; // Ska va 7 (förrförra noden)
 	
-	test_variable_a = p_robot_node->links[0].open; // Ska va 0
+	//test_variable_a = p_robot_node->links[2].open; // Ska va 1 // fungerar
+	//test_variable_b = p_robot_node->links[3].open; // Ska va 1 // fungerar
+	
+	robot_dir = 1;// Vi åker öster
+	p_robot_node = Create_node(5, 9, p_robot_node, robot_dir, 3, What_is_open(0, 0, 0, robot_dir)); // Vi har åkt höger och skapat en nod.
+	
+	/*
+	// ------------- Test hur många noder som får plats i minnet ------------- //
+	test_variable_a = 0;
+	while (malloc(sizeof(node)) != NULL)
+	{
+		test_variable_a = test_variable_a + 1;
+	}
+	
+	test_variable_b = p_robot_node->links[0].open; // Ska va 0
 	test_variable_b = p_robot_node->links[1].open; // Ska va 1
+	*/
 	
-	p_robot_node = Create_node(5, 9, p_robot_node, robot_dir, 3, What_is_open(1, 1, 0, robot_dir)); // Vi har åkt höger och skapat en nod.
 	/*
 	struct node node1;
 	struct node node2;
