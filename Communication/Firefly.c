9208d101feafa5de25fe72bb027ae89f9c09e3b6
Lina Karlsson <linka950@student.liu.se>
2014-05-02 12:58:59 +0200

    lagt till två filer för fireflykod samt skrivit initfunktion och avbrottsrutin

---
+++ Communication/Firefly.c
@@ -0,0 +1,30 @@
+﻿/*
+ * Firefly.c
+ *
+ * Created: 5/2/2014 12:00:11 PM
+ *  Author: linka950
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "../CommonLibrary/Common.h"
+#include "Communication_SPI.h"
+
+void USART_init()
+{	
+	DDRD = 0x08; //Sätter PD3 (USART1 transmitt pin) till utgång
+	UBRR1 = 0x09; //Sätter baudrate
+	
+	UCSR1A = 0x00;
+	UCSR1B = 0x98; //Möjliggör skickning och mottagning med USART, samt aktiverar avbrott från RXC1 flaggan
+	UCSR1C = 0x06; //Anger att vi vill skicka 8 bitar.
+}
+
+ISR(USART1_RX_vect)
+{
+	cli();
+	uint8_t command = UDR1;
+	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, command);
+	
+	sei();
+}
---
+++ Communication/Firefly.h
@@ -0,0 +1,16 @@
+﻿/*
+ * Firefly.h
+ *
+ * Created: 5/2/2014 12:11:14 PM
+ *  Author: linka950
+ */ 
+
+
+#ifndef FIREFLY_H_
+#define FIREFLY_H_
+
+void USART_init();
+
+
+
+#endif /* FIREFLY_H_ */
