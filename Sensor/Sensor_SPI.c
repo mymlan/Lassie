eed7d78f559fc8e4ec4f785d373493068553862b
henba136 <henba136@student.liu.se>
2014-05-05 13:50:01 +0200

    Funktionen SPI_sensor_send är utbytt till SPI_sensor_send_sensor_data och används endast för att skicka sensordata

--- Sensor/Sensor_SPI.c
+++ Sensor/Sensor_SPI.c
@@ -4,7 +4,6 @@
 #include "../CommonLibrary/Common.h"
 #include "Sensor_SPI.h"
 
-static volatile uint8_t master_has_recieved_byte;
 static volatile uint8_t has_recieved_give_ir_sensor_data;
 static volatile uint8_t has_recieved_give_distance;
 static volatile uint8_t has_recieved_start_angular_rate_sensor;
@@ -22,10 +21,7 @@ void SPI_sensor_init(void)
 	PORTD = 0; //test
 	PORTD = 0x20; //test
 	PORTD = 0; //test
-	
-	SPDR = 0x22;
 
-	master_has_recieved_byte = 0; //initera variabler
 	has_recieved_give_ir_sensor_data = 0;
 	has_recieved_give_distance = 0;
 	has_recieved_start_angular_rate_sensor = 0;
@@ -41,12 +37,6 @@ ISR(SPI_STC_vect)
 	uint8_t byte_from_SPI = SPDR;
 	switch (byte_from_SPI)
 	{
-		case MASTER_RECIEVED_BYTE:
-			master_has_recieved_byte = 1;
-			PORTD = 0;
-			PORTD = 0x20;
-			PORTD = 0;
-			break;
 		case ID_BYTE_GIVE_IR_SENSOR_DATA:
 			has_recieved_give_ir_sensor_data = 1;
 			test3 = 1;
@@ -66,12 +56,6 @@ ISR(SPI_STC_vect)
 	}
 }
 
-uint8_t SPI_master_have_recieved_byte(void)
-{
-	uint8_t result = master_has_recieved_byte;
-	master_has_recieved_byte = 0;
-	return result;
-}
 uint8_t SPI_sensor_should_give_ir_sensor_data(void)
 {
 	uint8_t result = has_recieved_give_ir_sensor_data;
@@ -109,24 +93,11 @@ void SPI_sensor_send_rotation_finished(void)
 /* void SPI_sensor_send(uint8_t id_byte, volatile uint8_t *data)
 *  Skickar hela meddelanden från sensor till Master
 */
-void SPI_sensor_send(uint8_t id_byte, uint8_t *data)
+void SPI_sensor_send_sensor_data(uint8_t *data)
 {
-	uint8_t number_of_bytes_in_data = 0;
-	switch (id_byte)
-	{
-		case ID_BYTE_IR_SENSOR_DATA:
-		number_of_bytes_in_data = NUMBER_OF_BYTES_IR_SENSOR_DATA;
-		break;
-		case ID_BYTE_DISTANCE: 
-		number_of_bytes_in_data = 1;
-		break;
-		default:
-		error = 1;
-		break;
-	}
-		
+	uint8_t number_of_bytes_in_data = NUMBER_OF_BYTES_IR_SENSOR_DATA;	
 	cli();
-	SPI_sensor_slave_send_id_byte(id_byte);
+	SPI_sensor_slave_send_id_byte(ID_BYTE_IR_SENSOR_DATA);
 		
 	while(number_of_bytes_in_data != 0)
 	{	
