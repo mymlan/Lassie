b5dc7dc87a6fa43420aeac6133e341987ffe20bf
henba136 <henba136@student.liu.se>
2014-05-05 14:31:12 +0200

    bytt namn på funktionerna

--- Communication/Firefly.c
+++ Communication/Firefly.c
@@ -30,39 +30,30 @@ ISR(USART1_RX_vect)
 	sei();
 }
 
-void USART_send_data_byte_to_PC(uint8_t data_byte)
+static void USART_transmit_byte_to_PC(uint8_t data_byte)
 {
 	while(!(UCSR1A & (1<<UDRE1))){} //Väntar tills UDR1 är redo att ta emot ny data	
 	UDR1 = data_byte;
 	_delay_us(10); //Testa om vi verkligen behöver en delay
 }
 
-void USART_send_sensor_data_to_PC(uint8_t id_byte, uint8_t *data_ptr)
+void USART_send_sensor_data_to_PC(uint8_t *data_ptr)
 {
-	uint8_t number_of_bytes_in_data = 0;
-	switch (id_byte)
-	{
-		case ID_BYTE_IR_SENSOR_DATA:
-		number_of_bytes_in_data = NUMBER_OF_BYTES_IR_SENSOR_DATA;
-		break;
-		default:
-		error = 1;
-		break;
-	}
+	uint8_t number_of_bytes_in_data = NUMBER_OF_BYTES_IR_SENSOR_DATA;
 	
 	cli();
-	USART_send_data_byte_to_PC(id_byte);
+	USART_transmit_byte_to_PC(ID_BYTE_IR_SENSOR_DATA);
 	for(int8_t i = (number_of_bytes_in_data - 1); i >= 0; i--)
 	{
-		USART_send_data_byte_to_PC(data_ptr[i]);
+		USART_transmit_byte_to_PC(data_ptr[i]);
 	}
 	sei();
 }
 
-void USART_send_one_sensor_value_to_PC(uint8_t id_byte, uint8_t sensor_value)
+void USART_send_byte_to_PC(uint8_t id_byte, uint8_t value)
 {
 	cli();
-	USART_send_data_byte_to_PC(id_byte);
-	USART_send_data_byte_to_PC(sensor_value);
+	USART_transmit_byte_to_PC(id_byte);
+	USART_transmit_byte_to_PC(value);
 	sei();
 }
--- Communication/Firefly.h
+++ Communication/Firefly.h
@@ -3,7 +3,7 @@
 #define FIREFLY_H_
 
 void USART_init();
-void USART_send_sensor_data_to_PC(uint8_t id_byte, uint8_t *data_ptr);
-void USART_send_one_sensor_value_to_PC(uint8_t id_byte, uint8_t sensor_value);
+void USART_send_sensor_data_to_PC(uint8_t *data_ptr);
+void USART_send_byte_to_PC(uint8_t id_byte, uint8_t sensor_value);
 
-#endif /* FIREFLY_H_ */
+#endif
