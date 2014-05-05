ef49540b9365ab891a7096d5a7eb734cfaac6b0d
unknown <henba136@student.liu.se>
2014-05-05 12:43:04 +0200

    ändrat namn på inparameter

--- Communication/Communication_SPI.h
+++ Communication/Communication_SPI.h
@@ -4,7 +4,7 @@
 
 void SPI_Master_init(void);
 
-void SPI_Master_send_to_sensor(uint8_t address_byte);
+void SPI_Master_send_to_sensor(uint8_t id_byte);
 void SPI_Master_send_to_steering(uint8_t id_byte, uint8_t *data_ptr);
 void SPI_Master_send_command_to_steering(uint8_t id_byte, uint8_t command);
 
