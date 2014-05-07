5f9725553d6fd70599f6e94cf3e2e30a6947de13
mymlan <mikaela.ivars@gmail.com>
2014-05-07 09:31:31 +0200

    rensat mainen

--- Communication/Communication.c
+++ Communication/Communication.c
@@ -23,54 +23,12 @@ int main(void)
 	//timer (hämta sensordata)
 	//kart-tjofräs
 	
-	//SPI_Master_send_to_sensor(ID_BYTE_GIVE_IR_SENSOR_DATA);
-	
-	//uint8_t test_sensor_data[7];
-	
-	/*test_sensor_data[0] = 10;//plockar ut värdet, alt stoppar in. 0-5
-	test_sensor_data[1] = 11;
-	test_sensor_data[2] = 12;
-	test_sensor_data[3] = 13;
-	test_sensor_data[4] = 14;
-	test_sensor_data[5] = 15;
-	test_sensor_data[6] = 3;
-	*/
-	/*SPI_Master_send_command_to_steering(ID_BYTE_AUTO_DECISIONS, COMMAND_FORWARD);
-	_delay_ms(1000);
-	
-	SPI_Master_send_to_sensor(ID_BYTE_GIVE_IR_SENSOR_DATA);
-	
-	//SPI_Master_send_to_steering(ID_BYTE_IR_SENSOR_DATA, test_sensor_data);
-	
-	_delay_ms(2000);
-	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, COMMAND_STOP);*/
-	
-	/*
-	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, COMMAND_FORWARD);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, COMMAND_BACKWARD);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_AUTO_DECISIONS, COMMAND_ROTATE_RIGHT);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_AUTO_DECISIONS, COMMAND_ROTATE_LEFT);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_AUTO_DECISIONS, COMMAND_CLOSE_CLAW);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_AUTO_DECISIONS, COMMAND_OPEN_CLAW);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, COMMAND_TURN_RIGHT);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, COMMAND_TURN_LEFT);
-	_delay_ms(1000);
-	SPI_Master_send_command_to_steering(ID_BYTE_MANUAL_DECISIONS, COMMAND_STOP);
-	*/
-	
 	SPI_Master_send_command_to_steering(ID_BYTE_AUTO_DECISIONS, COMMAND_FORWARD);
     while(1)
     {
 		_delay_ms(50);
 		
-		SPI_Master_send_to_sensor(ID_BYTE_GIVE_IR_SENSOR_DATA);
+		SPI_Master_send_id_byte_to_sensor(ID_BYTE_GIVE_IR_SENSOR_DATA);
     }
 	return 0;
 }
