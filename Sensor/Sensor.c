bafd4e4a4df241e42a629a76d2b83a3f46b0c8d7
mymlan <mikaela.ivars@gmail.com>
2014-05-11 10:37:25 +0200

    snyggat till swich-case samt tagit bort utkommenterad kod

--- Sensor/Sensor.c
+++ Sensor/Sensor.c
@@ -13,15 +13,6 @@ int main(void)
 	SPI_sensor_init();
 	sei();
 	
-	//För att kunna förbättre regleringen finns följande while
-	/*while(1)
-	{
-		if(SPI_sensor_should_give_ir_sensor_data())
-		{
-			SPI_sensor_send_sensor_data(ir_sensor_data);
-		}
-	}*/
-	
     while(1)
 	{
 		;
