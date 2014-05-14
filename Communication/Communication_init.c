373c573eac22305f2ad7df2a2a716052dd91ffe6
hugggos <hugjo456@student.liu.se>
2014-05-14 09:05:39 +0200

    lagt till initiering av knappar och skapar en nu fil communication_init.c

--- Communication/Communication_init.c
+++ Communication/Communication_init.c
@@ -1,17 +1,37 @@
-/*
- * CFile1.c
- *
- * Created: 2014-05-14 08:31:42
- *  Author: Hugo
- */ 
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "Communication_init.h"
+#include "../CommonLibrary/Common.h"
 
-void init_button ()
+void init_button_search ()
 {
 	DDRC &= ~(1 << DDC0);
 	PORTC |= (1 << PORTC0);
 	
 	PCICR |= (1 << PCIE2);
 	PCMSK2 |= (1 << PCINT16);
+}
+
+ISR(PCINT2_vect)
+{
+	//starta genom sökning av labyrint ??
+	PCICR |= (0 << PCIE2);
+	PCMSK2 |= (0 << PCINT16);
+}
+
+void init_button_deliver () //initieras när målet är funnet och genomsökningen är klar
+{
+	DDRD &= ~(1 << DDD4);
+	PORTD |= (1 << PORTD4);
+	
+	PCICR |= (1 << PCIE3);
+	PCMSK3 |= (1 << PCINT28);
+}
+
+ISR(PCINT3_vect)
+{
+	//klar med genomsökning och  starta leverans av vara
+	PCICR |= (0 << PCIE3);
+	PCMSK3 |= (0 << PCINT28);
 }
---
+++ Communication/Communication_init.h
@@ -0,0 +1,7 @@
+#ifndef _COMMUNICATION_INIT_H
+#define _COMMUNICATION_INIT_H
+
+void init_button_search ();
+void init_buttin_deliver ();
+
+#endif
