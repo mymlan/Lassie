f5ad9ebd3e7d6c1dd87154b7953b0a1c57f0dd2c
linka950 <linka950@student.liu.se>
2014-05-17 18:08:25 +0200

    brytit ut funktioner och fixat så att sensorvärden plottas i realtid

--- PC/mycallback.m
+++ PC/mycallback.m
@@ -1,162 +1,141 @@
- 
-function mycallback(hObject, eventdata, BT, sensor_data, number_of_sensor_data_collected, handles)
 
- %load('data_från_robot') %load och savwe fungerar, men känns som om det
- %blir trögt..
- id_byte = fread(BT,1);
- %disp(id_byte)
- switch id_byte 
-   case 1 %sensorvärden
-         for j = 1:7
-             %Ev lägga in en while här, men känns onödigt om felmeddelandet
-             %aldrig uppstår
-            byte = fread(BT,1);
-             switch j
-                 case 1
-                     sensor_data(j, number_of_sensor_data_collected) = byte;
-                     set(handles.error,'String',byte)
-                     drawnow()
-                 case 2
-                     sensor_data(j, number_of_sensor_data_collected) = byte;
-                     set(handles.angle,'String',byte)
-                     drawnow()
-                case 3
-                    sensor_data(j, number_of_sensor_data_collected) = byte;
-                    set(handles.ir_f,'String',byte)
-                    drawnow()
-                case 4
-                    sensor_data(j, number_of_sensor_data_collected) = byte;
-                    set(handles.ir_h_b,'String',byte)
-                    drawnow()
-                case 5
-                    sensor_data(j, number_of_sensor_data_collected) = byte;
-                    set(handles.ir_h_f,'String',byte)
-                    drawnow()
-                case 6
-                    sensor_data(j, number_of_sensor_data_collected) = byte;
-                    set(handles.ir_v_b,'String',byte)
-                    drawnow()
-                case 7
-                    sensor_data(j, number_of_sensor_data_collected) = byte;
-                    set(handles.ir_v_f,'String',byte)
-                    drawnow()
-                otherwise 
-                    disp('error i switch för sensordata')
-            end
-         end
-     number_of_sensor_data_collected = number_of_sensor_data_collected + 1;
+function mycallback(hObject, eventdata, BT, handles)
+global sensor_data;
+id_byte = fread(BT,1);
+disp(id_byte)
+switch id_byte
+    case 1 %sensorvärden
+        collect_and_plot_sensor_data(BT, handles);
     case 3 %distance
-        byte = fread(BT,1);
-        set(handles.distance,'String',byte)
+        distance = fread(BT,1);
+        set(handles.distance,'String',distance)
     case 8 %auto_decisions
-        set(handles.styrform, 'String', 'Autonom')
-        byte = fread(BT,1);
-        %disp(byte)
-        switch byte
-            case 16 %Stop
-                set(handles.kommando, 'String', 'Stanna')
-                drawnow() %Tvingar GUI att uppdatera sig
-            case 17 %Forward
-                set(handles.kommando, 'String', 'Fram')
-                drawnow()
-            case 18 %Backward
-                set(handles.kommando, 'String', 'Back')
-                drawnow()
-            case 19 %Turn Right
-                set(handles.kommando, 'String', 'Sväng höger')
-                drawnow()
-            case 20 %Turn left
-                set(handles.kommando, 'String', 'Sväng vänster')
-                drawnow()
-            case 21 %Rotate Right
-                set(handles.kommando, 'String', 'Rotera höger')
-                drawnow()
-            case 22 %Rotera vänster
-                set(handles.kommando, 'String', 'Rotera vänster')
-                drawnow()
-            case 23 %Close claw
-                set(handles.kommando, 'String', 'Greppa med gripklo')
-                drawnow()
-            case 24 %Open claw
-                set(handles.kommando, 'String', 'öppna gripklo')
-                drawnow()
-            case 25 %Forward not regulated
-                set(handles.kommando, 'String', 'Fram, ej reglerad')
-                drawnow()
-            case 26 %Backward not regulated
-                set(handles.kommando, 'String', 'Back, ej reglerad')
-                drawnow()
-            case 27 %Tight turn right
-                set(handles.kommando, 'String', 'Snäv högersväng')
-                drawnow()   
-            case 28 %Tight turn left
-                set(handles.kommando, 'String', 'Snäv vänstersväng')
-                drawnow() 
-            otherwise
-                disp('error i auto_decisions')
-        end
-    case 12 %Map coordinates
-        disp('uppdate map')
-        x_coordinate = fread(BT, 1);
-        y_coordinate = fread(BT, 1);
-        coordinates = [x_coordinate, y_coordinate];
-        disp(coordinates)
+        collect_and_display_steering_commands(BT, handles);
+    case 12 %Map links
+        %collect_and_plot_map_links(BT, handles)
     case 13 %Map parameters
-        for k = 1:7
-          byte = fread(BT,1);
-          switch k
-                case 1
-                    set(handles.robot_direction,'String',byte)
-                    drawnow()
-                case 2
-                    set(handles.size_all_nodes,'String',byte)
-                    drawnow()
-                case 3
-                    set(handles.following_path,'String',byte)
-                    drawnow()
-                case 4
-                    set(handles.enable_node_edit,'String',byte)
-                    drawnow()
-                case 5
-                    set(handles.level,'String',byte)
-                    drawnow()
-                case 6
-                    set(handles.x_coordinate,'String',byte)
-                    drawnow()
-                case 7
-                    set(handles.y_coordinate,'String',byte)
-                    drawnow()
-                otherwise 
-                    disp('error i switch för kartparametrar')
-          end
-        end
+        disp('update map')
+        collect_and_display_map_parameters(BT, handles);
     otherwise
         disp('error i switch för id-byte')
 end
 
-% start_value_to_plot_array = 1;
-% if(size(IR_front_long)> 100)
-%     start_value_to_plot_array = size(IR_front_long) - 100;
-% end
-% axes(handles.ir_front_long)
-% plot(IR_front_long(start_value_to_plot_array:size(IR_front_long)))
-% axes(handles.ir_right_back)
-% plot(IR_right_back(start_value_to_plot_array:size(IR_right_back)))
-% axes(handles.ir_right_front)
-% plot(IR_right_front(start_value_to_plot_array:size(IR_right_front)))
-% axes(handles.ir_left_back)
-% plot(IR_left_back(start_value_to_plot_array:size(IR_right_front)))
-% axes(handles.ir_left_front)
-% plot(IR_left_front(start_value_to_plot_array:size(IR_left_front)))
-% drawnow()
 
-% disp(IR_left_front)
-% disp(IR_left_back)
 
-% disp(IR_right_front)
-% disp(IR_right_back)
-% disp(IR_front_long)
-% disp(regulator_angle)
-% disp(regulator_error)
-%save('data_från_robot')
-%disp('all data du ville hämta är hämtad')
+function collect_and_plot_sensor_data(BT, handles)
+global sensor_data;
+new_sensor_data = fread(BT,7);
+new_IR_sensor_data = new_sensor_data(3:7);
+sensor_data = [sensor_data, new_IR_sensor_data]; %lägger till ett element sist i listan
+sensor_data(:,1) = []; %tar bort det första elementet i listan
+figure(handles.sensor_plot);
+plot(sensor_data');
+set(handles.error, 'string', new_sensor_data(1))
+set(handles.angle, 'string', new_sensor_data(2))
+set(handles.ir_f, 'string', new_sensor_data(3))
+set(handles.ir_h_b, 'string', new_sensor_data(4))
+set(handles.ir_h_f, 'string', new_sensor_data(5))
+set(handles.ir_v_b, 'string', new_sensor_data(6))
+set(handles.ir_v_f, 'string', new_sensor_data(7))
+
+
+
+function collect_and_plot_map_links(BT, handles)
+new_link = fread(BT, 4);
+handles.map_links = [handles.map_links; new_link];
+hold on
+for j = 1:size(handles.map_links, 1)
+    X1 = handles.map_links(j,1);
+    Y1 = handles.map_links(j,2);
+    X2 = handles.map_links(j,3);
+    Y2 = handles.map_links(j,4);
+    line([X1 X2], [Y1 Y2]);
+    figure(handles.map_plot);
+    plot([X1 X2], [Y1 Y2], 'o')
+end
+minx = min(min(handles.map_links(:,[1 3]))); %minsta värdet i alla rader kolumn 1 och 3
+miny = min(min(handles.map_links(:,[2 4])));
+maxx = max(max(handles.map_links(:,[1 3])));
+maxy = max(max(handles.map_links(:,[2 4])));
+axis([minx-1 maxx+1 miny-1 maxy+1]);
+hold off
+
+
+
+function collect_and_display_steering_commands(BT, handles)
+set(handles.styrform, 'String', 'Autonom')
+byte = fread(BT,1);
+%disp(byte)
+switch byte
+    case 16 %Stop
+        set(handles.kommando, 'String', 'Stanna')
+        drawnow() %Tvingar GUI att uppdatera sig
+    case 17 %Forward
+        set(handles.kommando, 'String', 'Fram')
+        drawnow()
+    case 18 %Backward
+        set(handles.kommando, 'String', 'Back')
+        drawnow()
+    case 19 %Turn Right
+        set(handles.kommando, 'String', 'Sväng höger')
+        drawnow()
+    case 20 %Turn left
+        set(handles.kommando, 'String', 'Sväng vänster')
+        drawnow()
+    case 21 %Rotate Right
+        set(handles.kommando, 'String', 'Rotera höger')
+        drawnow()
+    case 22 %Rotera vänster
+        set(handles.kommando, 'String', 'Rotera vänster')
+        drawnow()
+    case 23 %Close claw
+        set(handles.kommando, 'String', 'Greppa med gripklo')
+        drawnow()
+    case 24 %Open claw
+        set(handles.kommando, 'String', 'öppna gripklo')
+        drawnow()
+    case 25 %Forward not regulated
+        set(handles.kommando, 'String', 'Fram, ej reglerad')
+        drawnow()
+    case 26 %Backward not regulated
+        set(handles.kommando, 'String', 'Back, ej reglerad')
+        drawnow()
+    case 27 %Tight turn right
+        set(handles.kommando, 'String', 'Snäv högersväng')
+        drawnow()
+    case 28 %Tight turn left
+        set(handles.kommando, 'String', 'Snäv vänstersväng')
+        drawnow()
+    otherwise
+        disp('error i auto_decisions')
+end
+
+function collect_and_display_map_parameters(BT, handles)
+for k = 1:7
+    byte = fread(BT,1);
+    switch k
+        case 1
+            set(handles.robot_direction,'String',byte)
+            drawnow()
+        case 2
+            set(handles.size_all_nodes,'String',byte)
+            drawnow()
+        case 3
+            set(handles.following_path,'String',byte)
+            drawnow()
+        case 4
+            set(handles.enable_node_edit,'String',byte)
+            drawnow()
+        case 5
+            set(handles.level,'String',byte)
+            drawnow()
+        case 6
+            set(handles.x_coordinate,'String',byte)
+            drawnow()
+        case 7
+            set(handles.y_coordinate,'String',byte)
+            drawnow()
+        otherwise
+            disp('error i switch för kartparametrar')
+    end
+end
