6d2e144a6a5c6064ac0ebd56ea501311fc24a42c
mymlan <mikaela.ivars@gmail.com>
2014-05-20 12:13:51 +0200

    tagit bort frågefunk i main, inlagd i avbrott från SPI

--- Sensor/Sensor.c
+++ Sensor/Sensor.c
@@ -19,11 +19,7 @@ int main(void)
 	
     while(1)
 	{
-		if(SPI_sensor_should_handle_number_of_reflex_counts_to_RFID())
-		{
-			count_down_number_of_reflex_counts_to_RFID_requested = 1;
-			reflex_count = 0;
-		}
+		;
 	}
 	
 }
